08bb947a8de593b1dfd762be291f55b66c8081c2
i.polyakov-home <polyakovigo@yandex.ru>
2020-08-29 20:22:49 +0300

    feat(login): add login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,4 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
-export class AppComponent {
-    constructor() {
-        console.log('123');
-    }
-}
+export class AppComponent {}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.initializeForm();
+    }
+
+    private initializeForm(): void {
+        this.form = this.fb.group({
+            username: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+    }
+
+    onSubmit(): void {
+        console.log(this.form.valid, this.form.value);
+    }
+}
