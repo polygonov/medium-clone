17f70c8414d55aeef65b571a75e76ca42235a071
i.polyakov-home <polyakovigo@yandex.ru>
2020-08-29 21:42:13 +0300

    feat(auth): create store and action + interface

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from './../../store/actions/register.action';
 
 @Component({
     selector: 'app-register',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
     form: FormGroup;
 
-    constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder, private store: Store) {}
 
     ngOnInit(): void {
         this.initializeForm();
@@ -25,5 +27,6 @@ export class RegisterComponent implements OnInit {
 
     onSubmit(): void {
         console.log(this.form.valid, this.form.value);
+        this.store.dispatch(registerAction(this.form.value));
     }
 }
---
+++ src/app/shared/interfaces/currentUser.interface.ts
@@ -0,0 +1,11 @@
+export interface ICurrentUser {
+    id: number;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+    username: string;
+    bio: string | null;
+    image: string | null;
+    token: string;
+    password: string;
+}
